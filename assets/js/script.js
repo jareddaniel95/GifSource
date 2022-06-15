d447d73fb05819eec14f4334863cb307d32c6bbf
Jared Daniel <jareddaniel95@gmail.com>
2022-06-14 22:29:38 -0400

    Update results to show gifs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,41 @@
 var wordsAPIkey = '739c50b3f7mshd67eedcf81ee582p110fb9jsn185672be3425';
 var gihpyAPIkey = 'dAe5PgSXuoKvQSNB3cId7sn0DyvMK9VK';
 
+var synonymBlacklist = ['a', 'an', 'the', 'is', 'to', 'on', 'or', 'as', 'at', 'in', 'for', 'by', 'with'];
+
 var content = $('#content');
 var searchField = $('input[name="input"]');
 var buttonSearch = $('#search');
 
 buttonSearch.on('click', async function() {
-    var wordsResponse = await getSimilarWords(searchField.val());
+    // var wordsResponse = await getSimilarWords(searchField.val());
     var firstGiphyResponse = await getGifs(searchField.val());
     var giphyResponses = [];
     console.log(firstGiphyResponse);
     if (firstGiphyResponse != null) {
-        giphyResponses.push(firstGiphyResponse);
+        giphyResponses.push(firstGiphyResponse.data);
     }
-    if (wordsResponse != null) {
-        var synonyms = wordsResponse.synonyms;
-        for (var i = 0; i < Math.min(synonyms.length, 10); ++i) {
-            var giphyResponse = await getGifs(synonyms[i]);
-            console.log(giphyResponse);
-            if (giphyResponse != null) {
-                giphyResponses.push(giphyResponse);
+    console.log("data length: " + firstGiphyResponse.data.length);
+    console.log("input length: " + searchField.val().split(' ').length);
+    if (firstGiphyResponse.data.length <= 10 || searchField.val().split(' ').length == 1) {
+        var wordsResponse = await getSimilarWords(searchField.val());
+        if (wordsResponse != null) {
+            var synonyms = wordsResponse.synonyms;
+            for (var i = 0; i < Math.min(synonyms.length, 10); ++i) {
+                var giphyResponse = await getGifs(synonyms[i]);
+                console.log(giphyResponse);
+                if (giphyResponse != null && giphyResponse.data != null && giphyResponse.data.length > 0) {
+                    giphyResponses.push(giphyResponse.data);
+                }
             }
         }
     }
 
     for (var i = 0; i < giphyResponses.length; ++i) {
-        var test = $('<p>');
-        test.html(`<a href='${giphyResponses[i].data[0].url}'>${giphyResponses[i].data[0].title.trim() == '' ? 'Untitled' : giphyResponses[i].data[0].title}</a>`);
-        content.append(test);
+        var gif = $('<img>');
+        gif.attr('src', giphyResponses[i][0].images.fixed_height.url);
+        gif.attr('alt', `Result ${i}`);
+        content.append(gif);
     }
     
 });
