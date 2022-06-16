a919b6b254c1a49288c432944e0afd69dde09058
Jared Daniel <jareddaniel95@gmail.com>
2022-06-16 18:51:12 -0400

    updated gihpy api call logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ buttonSearch.on('click', async function() {
     var inputWords = searchField.val().split(' ');
     var firstGiphyResponse = await getGifsFromWord(searchField.val());
     var giphyResponses = [];
+    console.log(searchField.val());
     console.log(firstGiphyResponse);
     if (firstGiphyResponse != null && firstGiphyResponse.data != null && firstGiphyResponse.data.length > 0) {
         giphyResponses.push(firstGiphyResponse.data);
@@ -18,7 +19,7 @@ buttonSearch.on('click', async function() {
     console.log("data length: " + firstGiphyResponse.data.length);
     console.log("input length: " + searchField.val().split(' ').length);
     // CASE 1 - one word search
-    if (inputWords.length == 1) { //firstGiphyResponse.data.length <= 10 || 
+    if (inputWords.length == 1) {
         var wordsResponse = await getSimilarWords(searchField.val());
         if (wordsResponse != null) {
             var synonyms = wordsResponse.synonyms;
@@ -33,21 +34,32 @@ buttonSearch.on('click', async function() {
     }
     // CASE 2 - multi-word search
     else if(inputWords.length > 1) {
-        for (var k = 0; k < inputWords.length; ++k) {
-            // Search original word
-            var firstGiphyResponse = await getGifsFromWord(inputWords[k]);
-            if (firstGiphyResponse != null && firstGiphyResponse.data != null && firstGiphyResponse.data.length > 0) {
-                giphyResponses.push(firstGiphyResponse.data);
-            }
-            
-            // Search synonyms
+        for (var k = 0; k < Math.min(inputWords.length, 4); ++k) {
             if (!synonymBlacklist.includes(inputWords[k])) {
+            // Search original word
+                // var firstGiphyResponse = await getGifsFromWord(inputWords[k]);
+                // if (firstGiphyResponse != null && firstGiphyResponse.data != null && firstGiphyResponse.data.length > 0) {
+                //     giphyResponses.push(firstGiphyResponse.data);
+                // }
+            
+                // Search synonyms
                 var wordsResponse = await getSimilarWords(inputWords[k]);
                 if (wordsResponse != null) {
                     var synonyms = wordsResponse.synonyms;
-                    for (var i = 0; i < Math.min(synonyms.length, 4); ++i) {
-                        var giphyResponse = await getGifsFromWord(synonyms[i]);
-                        console.log(synonyms[i]);
+                    for (var i = 0; i < Math.min(synonyms.length, Math.max(6 - inputWords.length, 2)); ++i) {
+                        //var query = inputWords.join(' ');
+                        var query = '';
+                        for (var j = 0; j < inputWords.length; ++j) {
+                            if (j == k) {
+                                query += synonyms[i] + ' ';
+                            } else {
+                                query += inputWords[j] + ' ';
+                            }
+                        }
+                        // var giphyResponse = await getGifsFromWord(`${synonyms[i]} ${inputWords[k]}`);
+                        var giphyResponse = await getGifsFromWord(query.trim());
+                        // console.log(synonyms[i] + " " + inputWords[k]);
+                        console.log(query);
                         console.log(giphyResponse);
                         if (giphyResponse != null && giphyResponse.data != null && giphyResponse.data.length > 0) {
                             giphyResponses.push(giphyResponse.data);
@@ -61,7 +73,8 @@ buttonSearch.on('click', async function() {
     else {
         console.log("Nothing here");
     }
-
+    console.log(giphyResponses);
+    content.empty();
     for (var i = 0; i < giphyResponses.length; ++i) {
         var gif = $('<img>');
         gif.attr('src', giphyResponses[i][0].images.fixed_height.url);
@@ -102,8 +115,17 @@ async function getGifsFromWord(input) {
     return result;
 }
 
-async function getGifsFromArray(input) {
-    input.forEach(word => {
-
-    })
+async function translateWordToGif(input) {
+    var gihpyQuery = `https://api.giphy.com/v1/gifs/translate?api_key=${gihpyAPIkey}&s=${input}`;
+    let result = fetch(gihpyQuery)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            return null;
+        })
+        .then(function (data) {
+            return data;
+        });
+    return result;
 }
