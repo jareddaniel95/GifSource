686a09fcf69a4105501f27cd746a85cb20017987
Jared Daniel <jareddaniel95@gmail.com>
2022-06-17 15:37:35 -0400

    Make history items clickable, add headers to history and results sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,122 +8,33 @@ var searchField = $('input[name="input"]');
 var buttonSearch = $('#search');
 var searchBox = $('#searchbox');
 var historyItems = $('#historyItems');
+var loadingIcon = $('#loading-icon');
+var recentSearchesHeader = $('#recent-searches-h');
+var searchResultsHeader = $('#search-results-h');
 
 var pastGiphyResponses = JSON.parse(localStorage.getItem("PastGiphyResponses"));
 if (pastGiphyResponses) {
+    recentSearchesHeader.attr('style', 'display:block');
     pastGiphyResponses.forEach(pastResponse => {
         addHistoryItem(pastResponse);
     })
 }
 
-
-// buttonSearch.on('click', async function() {
-    // // Get GIFs
-    // var inputWords = searchField.val().split(' ');
-    // var firstGiphyResponse = await getGifsFromWord(searchField.val());
-    // var standardGiphyResponse = null;
-    // var altGiphyResponses = [];
-    // console.log(searchField.val());
-    // console.log(firstGiphyResponse);
-    // if (firstGiphyResponse != null && firstGiphyResponse.data != null && firstGiphyResponse.data.length > 0) {
-    //     standardGiphyResponse = firstGiphyResponse.data;
-    // }
-    // console.log("data length: " + firstGiphyResponse.data.length);
-    // console.log("input length: " + searchField.val().split(' ').length);
-    // // CASE 1 - one word search
-    // if (inputWords.length == 1) {
-    //     var wordsResponse = await getSimilarWords(searchField.val());
-    //     if (wordsResponse != null) {
-    //         var synonyms = wordsResponse.synonyms;
-    //         for (var i = 0; i < Math.min(synonyms.length, 10); ++i) {
-    //             var giphyResponse = await translateWordToGif(synonyms[i]);
-    //             console.log(giphyResponse);
-    //             if (giphyResponse != null && giphyResponse.data != null) {
-    //                 altGiphyResponses.push(giphyResponse.data.images);
-    //             }
-    //         }
-    //     }
-    // }
-    // // CASE 2 - multi-word search
-    // else if(inputWords.length > 1) {
-    //     for (var k = 0; k < Math.min(inputWords.length, 4); ++k) {
-    //         if (!synonymBlacklist.includes(inputWords[k])) {
-    //             // Search synonyms
-    //             var wordsResponse = await getSimilarWords(inputWords[k]);
-    //             if (wordsResponse != null) {
-    //                 var synonyms = wordsResponse.synonyms;
-    //                 for (var i = 0; i < Math.min(synonyms.length, Math.max(6 - inputWords.length, 2)); ++i) {
-    //                     var query = '';
-    //                     for (var j = 0; j < inputWords.length; ++j) {
-    //                         if (j == k) {
-    //                             query += synonyms[i] + ' ';
-    //                         } else {
-    //                             query += inputWords[j] + ' ';
-    //                         }
-    //                     }
-    //                     var giphyResponse = await translateWordToGif(query.trim());
-    //                     console.log(query);
-    //                     console.log(giphyResponse);
-    //                     if (giphyResponse != null && giphyResponse.data != null) {
-    //                         altGiphyResponses.push(giphyResponse.data.images);
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
-    // // CASE 3 - empty search
-    // else {
-    //     console.log("Nothing here");
-    // }
-    // console.log(altGiphyResponses);
-    // content.empty();
-
-    // // Render Gifs
-    // for (var i = 0; i < standardGiphyResponse.length; ++i) {
-    //     var gif = $('<img>');
-    //     gif.addClass('result-gif');
-    //     gif.attr('src', standardGiphyResponse[i].images.fixed_height.url);
-    //     gif.attr('alt', `Result ${i}`);
-    //     content.append(gif);
-    // }
-
-    // for (var i = 0; i < altGiphyResponses.length; ++i) {
-    //     var gif = $('<img>');
-    //     gif.addClass('result-gif');
-    //     gif.attr('src', altGiphyResponses[i].fixed_height.url);
-    //     gif.attr('alt', `Result ${i}`);
-    //     content.append(gif);
-    // }
-
-    // // Save to local storage
-    // var searchObj = {
-    //     'searchValue': searchField.val(),
-    //     'response': standardGiphyResponse[0].images.fixed_height_small_still.url
-    // }
-    // var list = JSON.parse(localStorage.getItem("PastGiphyResponses"));
-    // console.log(list);
-    // if (!list) {
-    //     list = [];
-    // } else {
-    //     if (list.length >= 8) {
-    //         var removedItem = list.shift();
-    //         var prevSearch = $(`#historyItems div:contains(${removedItem.searchValue})`);
-    //         //historyItems.remove(prevSearch); // Not working?
-    //         prevSearch.attr('style', 'display: none;');
-    //     }
-    // }
-    // list.push(searchObj);
-    // localStorage.setItem("PastGiphyResponses", JSON.stringify(list));
-
-    // addHistoryItem(searchObj);
-    
-// });
-
 buttonSearch.on('click', searchGifs);
+
 searchField.keypress(async function(e) {
     console.log("keypress()");
     if (e.keyCode == 13) {
+        searchGifs();
+    }
+});
+
+historyItems.on('click', function(event) {
+    var itemClicked = $(event.target);
+    console.log(itemClicked.text());
+    if (itemClicked.hasClass('custom-border')) {
+        console.log('true');
+        searchField.val(itemClicked.text());
         searchGifs();
     }
 });
@@ -133,6 +44,10 @@ async function searchGifs() {
     if (searchField.val().trim() === '') {
         return false;
     }
+
+    content.empty();
+    searchResultsHeader.attr('style', 'margin-left:auto; margin-right:auto; display:none');
+    loadingIcon.attr('style', 'margin-left:auto; margin-right:auto; display:block');
 
     // Get GIFs
     var inputWords = searchField.val().split(' ');
@@ -193,7 +108,9 @@ async function searchGifs() {
         console.log("Nothing here");
     }
     console.log(altGiphyResponses);
-    content.empty();
+    // content.empty();
+    loadingIcon.attr('style', 'margin-left: auto; margin-right: auto; display:none');
+    searchResultsHeader.attr('style', 'margin-left:auto; margin-right:auto; display:block');
 
     // Render Gifs
     for (var i = 0; i < standardGiphyResponse.length; ++i) {
@@ -205,11 +122,13 @@ async function searchGifs() {
     }
 
     for (var i = 0; i < altGiphyResponses.length; ++i) {
-        var gif = $('<img>');
-        gif.addClass('result-gif');
-        gif.attr('src', altGiphyResponses[i].fixed_height.url);
-        gif.attr('alt', `Result ${i}`);
-        content.append(gif);
+        if (altGiphyResponses[i]) {
+            var gif = $('<img>');
+            gif.addClass('result-gif');
+            gif.attr('src', altGiphyResponses[i].fixed_height.url);
+            gif.attr('alt', `Result ${i}`);
+            content.append(gif);
+        }
     }
 
     // Save to local storage
@@ -231,16 +150,16 @@ async function searchGifs() {
     }
     list.push(searchObj);
     localStorage.setItem("PastGiphyResponses", JSON.stringify(list));
-
+    recentSearchesHeader.attr('style', 'display:block');
     addHistoryItem(searchObj);
 }
 
 function addHistoryItem(pastResponse) {
     var newCol = $('<div>');
     newCol.addClass("col");
-    newCol.attr("class", "border custom-border m-2");
+    newCol.attr("class", "custom-border m-2 hoverable");
     newCol.attr("style", `width: 200px; height: 175px; display: flex; background-image: url(${pastResponse.response}); background-repeat: no-repeat; background-size: 100% 100%;`);
-    var historyName = $('<h2>');
+    var historyName = $('<h3>');
     historyName.attr("class", "text-center mb-0 historyHeader");
     historyName.attr("style", "align-self: flex-end; width: 100%;");
     historyName.text(pastResponse.searchValue);
