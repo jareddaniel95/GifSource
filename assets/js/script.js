b74c02b4755e1abb981d319ade0395cf55800772
Jared Daniel <jareddaniel95@gmail.com>
2022-06-17 14:02:57 -0400

    Search now triggered by pressing enter

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,130 @@ var searchBox = $('#searchbox');
 var historyItems = $('#historyItems');
 
 var pastGiphyResponses = JSON.parse(localStorage.getItem("PastGiphyResponses"));
-    if (pastGiphyResponses) {
-        pastGiphyResponses.forEach(pastResponse => {
-            addHistoryItem(pastResponse);
-        })
+if (pastGiphyResponses) {
+    pastGiphyResponses.forEach(pastResponse => {
+        addHistoryItem(pastResponse);
+    })
+}
+
+
+// buttonSearch.on('click', async function() {
+    // // Get GIFs
+    // var inputWords = searchField.val().split(' ');
+    // var firstGiphyResponse = await getGifsFromWord(searchField.val());
+    // var standardGiphyResponse = null;
+    // var altGiphyResponses = [];
+    // console.log(searchField.val());
+    // console.log(firstGiphyResponse);
+    // if (firstGiphyResponse != null && firstGiphyResponse.data != null && firstGiphyResponse.data.length > 0) {
+    //     standardGiphyResponse = firstGiphyResponse.data;
+    // }
+    // console.log("data length: " + firstGiphyResponse.data.length);
+    // console.log("input length: " + searchField.val().split(' ').length);
+    // // CASE 1 - one word search
+    // if (inputWords.length == 1) {
+    //     var wordsResponse = await getSimilarWords(searchField.val());
+    //     if (wordsResponse != null) {
+    //         var synonyms = wordsResponse.synonyms;
+    //         for (var i = 0; i < Math.min(synonyms.length, 10); ++i) {
+    //             var giphyResponse = await translateWordToGif(synonyms[i]);
+    //             console.log(giphyResponse);
+    //             if (giphyResponse != null && giphyResponse.data != null) {
+    //                 altGiphyResponses.push(giphyResponse.data.images);
+    //             }
+    //         }
+    //     }
+    // }
+    // // CASE 2 - multi-word search
+    // else if(inputWords.length > 1) {
+    //     for (var k = 0; k < Math.min(inputWords.length, 4); ++k) {
+    //         if (!synonymBlacklist.includes(inputWords[k])) {
+    //             // Search synonyms
+    //             var wordsResponse = await getSimilarWords(inputWords[k]);
+    //             if (wordsResponse != null) {
+    //                 var synonyms = wordsResponse.synonyms;
+    //                 for (var i = 0; i < Math.min(synonyms.length, Math.max(6 - inputWords.length, 2)); ++i) {
+    //                     var query = '';
+    //                     for (var j = 0; j < inputWords.length; ++j) {
+    //                         if (j == k) {
+    //                             query += synonyms[i] + ' ';
+    //                         } else {
+    //                             query += inputWords[j] + ' ';
+    //                         }
+    //                     }
+    //                     var giphyResponse = await translateWordToGif(query.trim());
+    //                     console.log(query);
+    //                     console.log(giphyResponse);
+    //                     if (giphyResponse != null && giphyResponse.data != null) {
+    //                         altGiphyResponses.push(giphyResponse.data.images);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // // CASE 3 - empty search
+    // else {
+    //     console.log("Nothing here");
+    // }
+    // console.log(altGiphyResponses);
+    // content.empty();
+
+    // // Render Gifs
+    // for (var i = 0; i < standardGiphyResponse.length; ++i) {
+    //     var gif = $('<img>');
+    //     gif.addClass('result-gif');
+    //     gif.attr('src', standardGiphyResponse[i].images.fixed_height.url);
+    //     gif.attr('alt', `Result ${i}`);
+    //     content.append(gif);
+    // }
+
+    // for (var i = 0; i < altGiphyResponses.length; ++i) {
+    //     var gif = $('<img>');
+    //     gif.addClass('result-gif');
+    //     gif.attr('src', altGiphyResponses[i].fixed_height.url);
+    //     gif.attr('alt', `Result ${i}`);
+    //     content.append(gif);
+    // }
+
+    // // Save to local storage
+    // var searchObj = {
+    //     'searchValue': searchField.val(),
+    //     'response': standardGiphyResponse[0].images.fixed_height_small_still.url
+    // }
+    // var list = JSON.parse(localStorage.getItem("PastGiphyResponses"));
+    // console.log(list);
+    // if (!list) {
+    //     list = [];
+    // } else {
+    //     if (list.length >= 8) {
+    //         var removedItem = list.shift();
+    //         var prevSearch = $(`#historyItems div:contains(${removedItem.searchValue})`);
+    //         //historyItems.remove(prevSearch); // Not working?
+    //         prevSearch.attr('style', 'display: none;');
+    //     }
+    // }
+    // list.push(searchObj);
+    // localStorage.setItem("PastGiphyResponses", JSON.stringify(list));
+
+    // addHistoryItem(searchObj);
+    
+// });
+
+buttonSearch.on('click', searchGifs);
+searchField.keypress(async function(e) {
+    console.log("keypress()");
+    if (e.keyCode == 13) {
+        searchGifs();
+    }
+});
+
+async function searchGifs() {
+    // Ignore when text field empty
+    if (searchField.val().trim() === '') {
+        return false;
     }
 
-buttonSearch.on('click', async function() {
-    
     // Get GIFs
     var inputWords = searchField.val().split(' ');
     var firstGiphyResponse = await getGifsFromWord(searchField.val());
@@ -117,8 +233,7 @@ buttonSearch.on('click', async function() {
     localStorage.setItem("PastGiphyResponses", JSON.stringify(list));
 
     addHistoryItem(searchObj);
-    
-});
+}
 
 function addHistoryItem(pastResponse) {
     var newCol = $('<div>');
